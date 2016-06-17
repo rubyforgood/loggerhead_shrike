896ab9185509731a34347b3f59a4174972fe296f
MichaelByrd <michaelbyrdsemail@gmail.com>
2016-06-17 14:11:03 -0400

    add phone and name to User model generate devise views for customization

--- db/migrate/20160617174555_devise_create_users.rb
+++ db/migrate/20160617174555_devise_create_users.rb
@@ -4,6 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name,               null: false, default: ""
+      t.string :phone,              null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
