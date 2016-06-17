20057476fe1928ad5342fa41c5dc6006b02d8ca1
MichaelByrd <michaelbyrdsemail@gmail.com>
2016-06-17 17:07:39 -0400

    move authenticate_user before_action

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,6 +1,15 @@
 class Users::InvitationsController < Devise::InvitationsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def edit
     puts '************'
     super
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone])
+  end
+
 end
