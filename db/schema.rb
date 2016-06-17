d68b62c63838cf10c23f2c861c85dcc0ba271105
MichaelByrd <michaelbyrdsemail@gmail.com>
2016-06-17 16:44:50 -0400

    rebase cleanup pt 2

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20160617180935) do
+=======
+ActiveRecord::Schema.define(version: 20160617181421) do
+>>>>>>> 4a6cde6... fix the order of the migrations
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+<<<<<<< HEAD
   create_table "observations", force: :cascade do |t|
     t.datetime "sighted_at"
     t.string   "location"
@@ -29,6 +34,13 @@ ActiveRecord::Schema.define(version: 20160617180935) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+=======
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "name",                   default: "", null: false
+    t.string   "phone",                  default: "", null: false
+>>>>>>> 4a6cde6... fix the order of the migrations
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
