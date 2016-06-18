c1f8d33d548f945aaf44abc9c4e2460165ce92bc
ahirschberg <alexhirschberg97@gmail.com>
2016-06-18 14:32:55 -0400

    Update to test :manage for user instances

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -2,15 +2,15 @@
 require 'cancan/matchers'
 
 RSpec.describe Ability, type: :model do
-
   it "can manage users when admin" do
     user = User.new :role => "admin"
     ability = Ability.new(user)
-    expect(ability).to be_able_to(:manage, User)
+    expect(ability).to be_able_to(:manage, User.new)
   end
+
   it "cannot manage user when not admin" do
     user = User.new
     ability = Ability.new(user)
-    expect(ability).not_to be_able_to(:manage, User)
+    expect(ability).not_to be_able_to(:manage, User.new)
   end
 end
