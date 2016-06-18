50d206c6cd8cc968a06ed3ef48460d20d6c909b5
Adina Bianchi <curiousloop@Adinas-MacBook-Air.local>
2016-06-18 11:16:59 -0400

    spec passes

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'jbuilder', '~> 2.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 gem 'rollbar'
+gem 'pry'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- spec/views/observations/index.html.erb_spec.rb
+++ spec/views/observations/index.html.erb_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
-
+require 'pry'
 RSpec.describe "observations/index", type: :view do
   before(:each) do
     assign(:observations, [
       Observation.create!(
         :location => "Location",
+        :sighted_at => DateTime.new(2016, 5, 10, 10, 30, 0, '-5'),
         :latitude => 10.99,
         :longitude => 11.99,
         :num_bands => 0
       ),
       Observation.create!(
-        :location => "Location Here",
+        :location => "here",
+        :sighted_at => DateTime.new(2014, 5, 10, 10, 40, 0, '-5'),
         :latitude => 12.99,
         :longitude => 13.99,
         :num_bands => 1
@@ -20,7 +22,8 @@ RSpec.describe "observations/index", type: :view do
 
   it "renders a list of observations" do
     render
-    assert_select "tr>td.location", :text => "Location".to_s, :count => 1
+    assert_select "tr>td.sighted_at_date", :text => "2016-05-10", :count => 1
+    assert_select "tr>td.sighted_at_time", :text => "15:30", :count => 1
     assert_select "tr>td.longitude", :text => "11.99".to_s, :count => 1
     assert_select "tr>td.latitude", :text => "10.99".to_s, :count => 1
     assert_select "tr>td.num_bands", :text => "1".to_s, :count => 1
