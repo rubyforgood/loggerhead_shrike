a657e9175358d1306bf771b3ca87aa6f1a60429a
ahirschberg <alexhirschberg97@gmail.com>
2016-06-18 14:25:56 -0400

    Added manage ability admin role, added tests

---
+++ spec/models/ability_spec.rb
@@ -0,0 +1,16 @@
+﻿require 'rails_helper'
+require 'cancan/matchers'
+
+RSpec.describe Ability, type: :model do
+
+  it "can manage users when admin" do
+    user = User.new :role => "admin"
+    ability = Ability.new(user)
+    expect(ability).to be_able_to(:manage, User)
+  end
+  it "cannot manage user when not admin" do
+    user = User.new
+    ability = Ability.new(user)
+    expect(ability).not_to be_able_to(:manage, User)
+  end
+end
