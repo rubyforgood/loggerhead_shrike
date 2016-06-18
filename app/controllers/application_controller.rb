c8f9ba9f57ecb8829946cd2c83a7121a7c02e24d
Melissa Miller <lis.d.miller@gmail.com>
2016-06-18 16:49:38 -0400

    edit user authorizations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception, prepend: true
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   rescue_from CanCan::AccessDenied do |exception|
       respond_to do |format|
         format.html { redirect_to root_path, :alert => "Unauthorized"}
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  load_and_authorize_resource
+  load_and_authorize_resource 
+
 
   def index
     @users = User.all
@@ -12,5 +13,16 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
     @resource = @user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    redirect_to users_path, alert: 'Success!' if @user.update_attributes(user_params)
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :name, :phone, :role)
   end
 end
