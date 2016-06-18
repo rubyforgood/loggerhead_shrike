1db3a61d8dc35235adc8dcdc7381ed3ab49fa037
Melissa Miller <lis.d.miller@gmail.com>
2016-06-18 14:38:14 -0400

    Refines restrictions to admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :check_admin, only: [:index]
+  # before_action :check_admin, only: [:index]
 
   def index
     @users = User.all
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,37 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
+  # describe "Limit access to user index" do
+  #   let(:admin) {
+  #     User.new(
+  #       email:                 "spacejame@gmail.com",
+  #       name:                  "Michael Jordan",
+  #       role:                  "admin",
+  #       password:              "password",
+  #       password_confirmation: "password"
+  #     )
+  #   }
+  #
+  #   let(:nonadmin) {
+  #     User.new(
+  #       email:                 "spacejame@gmail.com",
+  #       name:                  "Michael Jordan",
+  #       role:                  "scientist",
+  #       password:              "password",
+  #       password_confirmation: "password"
+  #     )
+  #   }
+  #
+  #   it "admin users have access to user index" do
+  #
+  #   end
+  #
+  #   it "non admin users do not have access to user index" do
+  #
+  #   end
+  #
+  # end
+
+
+
 end
