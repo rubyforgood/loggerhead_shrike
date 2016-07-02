04aa1f56c0c29b4b4edf70ef786ae9a10f23b809
Melissa Miller <lis.d.miller@gmail.com>
2016-07-02 10:30:36 -0400

    adds test for soft delete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,27 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
-  it "can be valid" do
-    user = User.new(
+  let(:user) do
+    User.new(
       email:                 "spacejam@gmail.com",
       name:                  "Michael Jordan",
       role:                  "admin",
       password:              "password",
       password_confirmation: "password"
     )
+  end
+
+  it "can be valid" do
     expect(user).to be_valid
   end
 
   it "can be an admin" do
-    user = User.new(
-      email:                 "spacejam@gmail.com",
-      name:                  "Michael Jordan",
-      role:                  "admin",
-      password:              "password",
-      password_confirmation: "password"
-    )
     expect(user.is_admin?).to be(true)
   end
 
+  describe "#destroy" do
+    let(:a_user) do
+      User.new(
+        email:                 "test@gmail.com",
+        name:                  "Michael Jordan",
+        role:                  "researcher",
+        password:              "password",
+        password_confirmation: "password"
+      )
+    end
+
+    before do
+      a_user.save
+    end
+
+    it "can be destroyed" do
+      a_user.destroy
+      expect(User.without_deleted).to be_empty
+      expect(User.only_deleted).not_to be_empty
+    end
+
+  end
 end
